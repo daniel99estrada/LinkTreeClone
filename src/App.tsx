713616b69cda551c9320e5908c53d2358b7b94ff
daniel99estrada <pildoestrada@gmail.com>
2023-08-08 18:03:29 -0400

    new changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,41 @@
 import React, { useState, useEffect } from 'react';
 import Link from './Link';
 import AddLink from './AddLink';
+import axios from 'axios';
+import ApiCaller from './ApiCaller';
+
+const apiUrl = 'https://ttmoh9fsnb.execute-api.us-east-1.amazonaws.com/dev/links';
 
 function App() {
   const [links, setLinks] = useState<{ name: string; url: string }[]>([]);
+  const [loading, setLoading] = useState<boolean>(true);
+
+  useEffect(() => {
+    // Make the API call and get the response data
+    const fetchData = async () => {
+      try {
+        const response = await axios.get(apiUrl);
+        // Assuming the response data is an array of objects as shown in your example
+        const responseData = response.data;
+        setLinks([...links, responseData]);
+        setLoading(false); // Set loading to false once data is fetched
+      } catch (error) {
+        console.error('Error fetching data:', error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  useEffect(() => {
+    // This useEffect will run whenever the 'links' state is updated
+    console.log('Updated links:', links);
+  }, [links]); // The second argument is the dependency array with 'links'
 
   const addLink = (name: string, url: string) => {
     const newLink = { name, url };
     setLinks([...links, newLink]);
   };
-
-  useEffect(() => {
-    console.log('Loading links from localStorage');
-    const storedLinks = localStorage.getItem('links');
-    if (storedLinks) {
-      setLinks(JSON.parse(storedLinks));
-    }
-  }, []);
-
-  useEffect(() => {
-    console.log('Saving links to localStorage');
-    localStorage.setItem('links', JSON.stringify(links));
-  }, [links]);
 
   const deleteLink = (index: number) => {
     // Remove the link at the given index from the links state
@@ -32,16 +46,21 @@ function App() {
 
   return (
     <div>
-      <h1>Hello</h1>
+      <h1>Demo</h1>
+      <ApiCaller />
       <AddLink onAddLink={addLink} />
-      {links.map((link, index) => (
-        <Link
-          key={index}
-          linkUrl={link.url}
-          linkName={link.name}
-          onDelete={() => deleteLink(index)} // Pass the deleteLink function as onDelete prop
-        />
-      ))}
+      {loading ? (
+        <p>Loading...</p>
+      ) : (
+        links.map((link, index) => (
+          <Link
+            key={index}
+            linkUrl={link.url}
+            linkName={link.name}
+            onDelete={() => deleteLink(index)} // Pass the deleteLink function as onDelete prop
+          />
+        ))
+      )}
     </div>
   );
 }
